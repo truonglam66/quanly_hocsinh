2d97537292a6a38ddc3e09feedf43c8bbf8f24c2
Lam Truong <truongg166@gmail.com>
2021-12-23 13:20:26 +0700

    merge develop

---
+++ QuanLyHocSinh/Forms/DanhSachBangDiem.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace QuanLyHocSinh
+{
+    partial class DanhSachBangDiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnTimkiem = new QuanLyHocSinh.CustomControl.CustomButton();
+            this.txbLop = new QuanLyHocSinh.CustomControl.CustomTextBox();
+            this.txbNamhoc = new QuanLyHocSinh.CustomControl.CustomTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.panel1.Controls.Add(this.btnTimkiem);
+            this.panel1.Controls.Add(this.txbLop);
+            this.panel1.Controls.Add(this.txbNamhoc);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(566, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(234, 411);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnTimkiem
+            // 
+            this.btnTimkiem.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnTimkiem.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnTimkiem.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnTimkiem.BorderRadius = 20;
+            this.btnTimkiem.BorderSize = 0;
+            this.btnTimkiem.FlatAppearance.BorderSize = 0;
+            this.btnTimkiem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTimkiem.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimkiem.ForeColor = System.Drawing.Color.White;
+            this.btnTimkiem.Location = new System.Drawing.Point(51, 276);
+            this.btnTimkiem.Name = "btnTimkiem";
+            this.btnTimkiem.Size = new System.Drawing.Size(145, 40);
+            this.btnTimkiem.TabIndex = 1;
+            this.btnTimkiem.Text = "Tìm kiếm";
+            this.btnTimkiem.TextColor = System.Drawing.Color.White;
+            this.btnTimkiem.UseVisualStyleBackColor = false;
+            this.btnTimkiem.Click += new System.EventHandler(this.btnTimkiem_Click);
+            // 
+            // txbLop
+            // 
+            this.txbLop.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.txbLop.BorderColor = System.Drawing.Color.CornflowerBlue;
+            this.txbLop.BorderFocusColor = System.Drawing.Color.DarkOrange;
+            this.txbLop.BorderRadius = 15;
+            this.txbLop.BorderSize = 2;
+            this.txbLop.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbLop.ForeColor = System.Drawing.Color.MintCream;
+            this.txbLop.Location = new System.Drawing.Point(51, 168);
+            this.txbLop.Margin = new System.Windows.Forms.Padding(4);
+            this.txbLop.Multiline = false;
+            this.txbLop.Name = "txbLop";
+            this.txbLop.Padding = new System.Windows.Forms.Padding(7);
+            this.txbLop.PasswordChar = false;
+            this.txbLop.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txbLop.PlaceholderText = "Lớp";
+            this.txbLop.Size = new System.Drawing.Size(145, 32);
+            this.txbLop.TabIndex = 0;
+            this.txbLop.Texts = "";
+            this.txbLop.UnderlinedStyle = false;
+            // 
+            // txbNamhoc
+            // 
+            this.txbNamhoc.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.txbNamhoc.BorderColor = System.Drawing.Color.CornflowerBlue;
+            this.txbNamhoc.BorderFocusColor = System.Drawing.Color.DarkOrange;
+            this.txbNamhoc.BorderRadius = 15;
+            this.txbNamhoc.BorderSize = 2;
+            this.txbNamhoc.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNamhoc.ForeColor = System.Drawing.Color.MintCream;
+            this.txbNamhoc.Location = new System.Drawing.Point(51, 89);
+            this.txbNamhoc.Margin = new System.Windows.Forms.Padding(4);
+            this.txbNamhoc.Multiline = false;
+            this.txbNamhoc.Name = "txbNamhoc";
+            this.txbNamhoc.Padding = new System.Windows.Forms.Padding(7);
+            this.txbNamhoc.PasswordChar = false;
+            this.txbNamhoc.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txbNamhoc.PlaceholderText = "Năm học";
+            this.txbNamhoc.Size = new System.Drawing.Size(145, 32);
+            this.txbNamhoc.TabIndex = 0;
+            this.txbNamhoc.Texts = "";
+            this.txbNamhoc.UnderlinedStyle = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.DarkSlateBlue;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.GridColor = System.Drawing.Color.DarkSlateBlue;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(566, 411);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // DanhSachBangDiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 411);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DanhSachBangDiem";
+            this.Text = "DanhSachBangDiem";
+            this.Load += new System.EventHandler(this.DanhSachBangDiem_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private CustomControl.CustomTextBox txbNamhoc;
+        private CustomControl.CustomTextBox txbLop;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private CustomControl.CustomButton btnTimkiem;
+    }
+}
---
+++ QuanLyHocSinh/Forms/HoatDongNgoaiGio.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace QuanLyHocSinh.Forms
+{
+    partial class HoatDongNgoaiGio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HoatDongNgoaiGio));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnHuy = new QuanLyHocSinh.CustomControl.CustomButton();
+            this.btnXoa = new QuanLyHocSinh.CustomControl.CustomButton();
+            this.btnSua = new QuanLyHocSinh.CustomControl.CustomButton();
+            this.btnThem = new QuanLyHocSinh.CustomControl.CustomButton();
+            this.btnDsHocSinh = new QuanLyHocSinh.CustomControl.CustomButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbDiaDiem = new QuanLyHocSinh.CustomControl.CustomTextBox();
+            this.txbGiaoVien = new QuanLyHocSinh.CustomControl.CustomTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbMaHoatDong = new QuanLyHocSinh.CustomControl.CustomTextBox();
+            this.DatePicker = new QuanLyHocSinh.CustomControl.CustomDateTimePicker();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dgvHDNG = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHDNG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.panel1.Controls.Add(this.btnHuy);
+            this.panel1.Controls.Add(this.btnXoa);
+            this.panel1.Controls.Add(this.btnSua);
+            this.panel1.Controls.Add(this.btnThem);
+            this.panel1.Controls.Add(this.btnDsHocSinh);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txbDiaDiem);
+            this.panel1.Controls.Add(this.txbGiaoVien);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txbMaHoatDong);
+            this.panel1.Controls.Add(this.DatePicker);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(436, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(321, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnHuy.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnHuy.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnHuy.BorderRadius = 20;
+            this.btnHuy.BorderSize = 0;
+            this.btnHuy.FlatAppearance.BorderSize = 0;
+            this.btnHuy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHuy.ForeColor = System.Drawing.Color.White;
+            this.btnHuy.Location = new System.Drawing.Point(171, 368);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(110, 40);
+            this.btnHuy.TabIndex = 5;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.TextColor = System.Drawing.Color.White;
+            this.btnHuy.UseVisualStyleBackColor = false;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnXoa.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnXoa.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnXoa.BorderRadius = 20;
+            this.btnXoa.BorderSize = 0;
+            this.btnXoa.FlatAppearance.BorderSize = 0;
+            this.btnXoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXoa.ForeColor = System.Drawing.Color.White;
+            this.btnXoa.Location = new System.Drawing.Point(29, 368);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(110, 40);
+            this.btnXoa.TabIndex = 5;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.TextColor = System.Drawing.Color.White;
+            this.btnXoa.UseVisualStyleBackColor = false;
+            // 
+            // btnSua
+            // 
+            this.btnSua.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnSua.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnSua.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnSua.BorderRadius = 20;
+            this.btnSua.BorderSize = 0;
+            this.btnSua.FlatAppearance.BorderSize = 0;
+            this.btnSua.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSua.ForeColor = System.Drawing.Color.White;
+            this.btnSua.Location = new System.Drawing.Point(171, 304);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(110, 40);
+            this.btnSua.TabIndex = 5;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.TextColor = System.Drawing.Color.White;
+            this.btnSua.UseVisualStyleBackColor = false;
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnThem.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnThem.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnThem.BorderRadius = 20;
+            this.btnThem.BorderSize = 0;
+            this.btnThem.FlatAppearance.BorderSize = 0;
+            this.btnThem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThem.ForeColor = System.Drawing.Color.White;
+            this.btnThem.Location = new System.Drawing.Point(29, 304);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(110, 40);
+            this.btnThem.TabIndex = 5;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.TextColor = System.Drawing.Color.White;
+            this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnDsHocSinh
+            // 
+            this.btnDsHocSinh.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnDsHocSinh.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnDsHocSinh.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnDsHocSinh.BorderRadius = 20;
+            this.btnDsHocSinh.BorderSize = 0;
+            this.btnDsHocSinh.FlatAppearance.BorderSize = 0;
+            this.btnDsHocSinh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDsHocSinh.ForeColor = System.Drawing.Color.White;
+            this.btnDsHocSinh.Image = ((System.Drawing.Image)(resources.GetObject("btnDsHocSinh.Image")));
+            this.btnDsHocSinh.Location = new System.Drawing.Point(265, 234);
+            this.btnDsHocSinh.Name = "btnDsHocSinh";
+            this.btnDsHocSinh.Size = new System.Drawing.Size(47, 35);
+            this.btnDsHocSinh.TabIndex = 5;
+            this.btnDsHocSinh.TextColor = System.Drawing.Color.White;
+            this.btnDsHocSinh.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.label3.ForeColor = System.Drawing.Color.MintCream;
+            this.label3.Location = new System.Drawing.Point(15, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Địa điểm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.label2.ForeColor = System.Drawing.Color.MintCream;
+            this.label2.Location = new System.Drawing.Point(15, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Giáo viên phụ trách";
+            // 
+            // txbDiaDiem
+            // 
+            this.txbDiaDiem.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.txbDiaDiem.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.txbDiaDiem.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.txbDiaDiem.BorderRadius = 0;
+            this.txbDiaDiem.BorderSize = 2;
+            this.txbDiaDiem.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.txbDiaDiem.ForeColor = System.Drawing.Color.MintCream;
+            this.txbDiaDiem.Location = new System.Drawing.Point(138, 157);
+            this.txbDiaDiem.Margin = new System.Windows.Forms.Padding(4);
+            this.txbDiaDiem.Multiline = false;
+            this.txbDiaDiem.Name = "txbDiaDiem";
+            this.txbDiaDiem.Padding = new System.Windows.Forms.Padding(7);
+            this.txbDiaDiem.PasswordChar = false;
+            this.txbDiaDiem.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txbDiaDiem.PlaceholderText = "";
+            this.txbDiaDiem.Size = new System.Drawing.Size(174, 33);
+            this.txbDiaDiem.TabIndex = 3;
+            this.txbDiaDiem.Texts = "";
+            this.txbDiaDiem.UnderlinedStyle = true;
+            // 
+            // txbGiaoVien
+            // 
+            this.txbGiaoVien.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.txbGiaoVien.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.txbGiaoVien.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.txbGiaoVien.BorderRadius = 0;
+            this.txbGiaoVien.BorderSize = 2;
+            this.txbGiaoVien.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.txbGiaoVien.ForeColor = System.Drawing.Color.MintCream;
+            this.txbGiaoVien.Location = new System.Drawing.Point(137, 98);
+            this.txbGiaoVien.Margin = new System.Windows.Forms.Padding(4);
+            this.txbGiaoVien.Multiline = false;
+            this.txbGiaoVien.Name = "txbGiaoVien";
+            this.txbGiaoVien.Padding = new System.Windows.Forms.Padding(7);
+            this.txbGiaoVien.PasswordChar = false;
+            this.txbGiaoVien.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txbGiaoVien.PlaceholderText = "";
+            this.txbGiaoVien.Size = new System.Drawing.Size(175, 33);
+            this.txbGiaoVien.TabIndex = 3;
+            this.txbGiaoVien.Texts = "";
+            this.txbGiaoVien.UnderlinedStyle = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.label1.ForeColor = System.Drawing.Color.MintCream;
+            this.label1.Location = new System.Drawing.Point(15, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mã hoạt động";
+            // 
+            // txbMaHoatDong
+            // 
+            this.txbMaHoatDong.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.txbMaHoatDong.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.txbMaHoatDong.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.txbMaHoatDong.BorderRadius = 0;
+            this.txbMaHoatDong.BorderSize = 2;
+            this.txbMaHoatDong.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.txbMaHoatDong.ForeColor = System.Drawing.Color.MintCream;
+            this.txbMaHoatDong.Location = new System.Drawing.Point(137, 31);
+            this.txbMaHoatDong.Margin = new System.Windows.Forms.Padding(4);
+            this.txbMaHoatDong.Multiline = false;
+            this.txbMaHoatDong.Name = "txbMaHoatDong";
+            this.txbMaHoatDong.Padding = new System.Windows.Forms.Padding(7);
+            this.txbMaHoatDong.PasswordChar = false;
+            this.txbMaHoatDong.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txbMaHoatDong.PlaceholderText = "";
+            this.txbMaHoatDong.Size = new System.Drawing.Size(175, 33);
+            this.txbMaHoatDong.TabIndex = 1;
+            this.txbMaHoatDong.Texts = "";
+            this.txbMaHoatDong.UnderlinedStyle = true;
+            // 
+            // DatePicker
+            // 
+            this.DatePicker.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.DatePicker.BorderSize = 0;
+            this.DatePicker.CustomFormat = "";
+            this.DatePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.DatePicker.Location = new System.Drawing.Point(18, 232);
+            this.DatePicker.MinimumSize = new System.Drawing.Size(4, 35);
+            this.DatePicker.Name = "DatePicker";
+            this.DatePicker.Size = new System.Drawing.Size(234, 35);
+            this.DatePicker.SkinColor = System.Drawing.Color.MediumSlateBlue;
+            this.DatePicker.TabIndex = 0;
+            this.DatePicker.TextColor = System.Drawing.Color.White;
+            this.DatePicker.Value = new System.DateTime(2021, 11, 3, 0, 0, 0, 0);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.panel2.Controls.Add(this.dgvHDNG);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(436, 450);
+            this.panel2.TabIndex = 2;
+            // 
+            // dgvHDNG
+            // 
+            this.dgvHDNG.BackgroundColor = System.Drawing.Color.DarkSlateBlue;
+            this.dgvHDNG.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvHDNG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.DarkSlateBlue;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.MintCream;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvHDNG.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvHDNG.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvHDNG.GridColor = System.Drawing.Color.DarkSlateBlue;
+            this.dgvHDNG.Location = new System.Drawing.Point(0, 0);
+            this.dgvHDNG.MultiSelect = false;
+            this.dgvHDNG.Name = "dgvHDNG";
+            this.dgvHDNG.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvHDNG.Size = new System.Drawing.Size(436, 450);
+            this.dgvHDNG.TabIndex = 0;
+            this.dgvHDNG.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHDNG_CellClick);
+            // 
+            // HoatDongNgoaiGio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "HoatDongNgoaiGio";
+            this.Text = "HoatDongNgoaiGio";
+            this.Load += new System.EventHandler(this.HoatDongNgoaiGio_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHDNG)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private CustomControl.CustomButton btnThem;
+        private CustomControl.CustomButton btnDsHocSinh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private CustomControl.CustomTextBox txbDiaDiem;
+        private CustomControl.CustomTextBox txbGiaoVien;
+        private System.Windows.Forms.Label label1;
+        private CustomControl.CustomTextBox txbMaHoatDong;
+        private CustomControl.CustomDateTimePicker DatePicker;
+        private CustomControl.CustomButton btnHuy;
+        private CustomControl.CustomButton btnXoa;
+        private CustomControl.CustomButton btnSua;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgvHDNG;
+    }
+}
